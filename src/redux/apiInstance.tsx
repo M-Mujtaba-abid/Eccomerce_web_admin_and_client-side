9b0664fd58c46a74f0d350cbfa282679dc735fad
Mujtaba Abid <mujtabaabid06@gmail.com>
2025-09-05 15:57:49 +0500

    backend deployed

--- src/redux/apiInstance.tsx
+++ src/redux/apiInstance.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000", 
+  // baseURL: "http://localhost:8000", 
+  baseURL: "https://eccomerce-web-server-side.onrender.com", 
   withCredentials: true, 
   
 });
