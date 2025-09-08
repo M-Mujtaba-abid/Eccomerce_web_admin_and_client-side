29714f29ef0b0f6b947bb0e7c1014be5f885edfd
Mujtaba Abid <mujtabaabid06@gmail.com>
2025-09-08 17:10:27 +0500

    cahnges in apiInstance for running on both local and hosted

--- src/redux/apiInstance.tsx
+++ src/redux/apiInstance.tsx
@@ -1,12 +1,17 @@
 import axios from "axios";
 
-const API = axios.create({
-  // baseURL: "http://localhost:8000" , 
-  baseURL: "https://eccomerce-web-server-side.onrender.com", 
-  withCredentials: true, 
+// const API = axios.create({
+//   // baseURL: "http://localhost:8000" , 
+//   baseURL: "https://eccomerce-web-server-side.onrender.com || http://localhost:8000 ", 
+//   withCredentials: true, 
   
+// });
+
+const API = axios.create({
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:8000"
+    : "https://eccomerce-web-server-side.onrender.com",
+  withCredentials: true,
 });
-
-
 
 export default API;
